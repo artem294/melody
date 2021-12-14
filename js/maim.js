07270dbec3f4e62f8a5641257f681bb82d2bc416
artem294 <95941269+artem294@users.noreply.github.com>
2021-12-14 17:47:33 +0300

    Add files via upload

--- js/maim.js
+++ js/maim.js
@@ -3,6 +3,9 @@ $(document).ready(function () {
     var floorPath = $(".home-image path"); // каждый отдельный этаж в  SVG
     var counterUp = $(".counter-up"); //кнопка увелечения этажа
     var counterDown = $(".counter-down"); //кнопка уменьшения этажа
+    var modal = $(".modal");
+    var modalCloseButton = $(".modal-close-button");
+    var viewFlatsButton = $(".view-flats");
 
     // функция при наведении мышью на этаж
     floorPath.on("mouseover", function () {
@@ -10,12 +13,17 @@ $(document).ready(function () {
         currentFloor = $(this).attr("data-floor"); // получаем значение текущего этажа
         $(".counter").text(currentFloor); // записываем значение этажа в счётчик справа 
     });
+
+     floorPath.on("click", toggleModel) /* при клике на этаж вызвать окно */
+
+     modalCloseButton.on("click", toggleModel); /* клик на кнопку закрыть убирает окно */
+     viewFlatsButton.on("click", toggleModel);
     counterUp.on("click", function() { // отслеживаем клик по кнопке вверх
      if(currentFloor < 18) { // проверяем значение этажа, оно не должно быть больше 18
         currentFloor++; // прибавляем 1 этаж
         usCurrentFloor = currentFloor.toLocaleString("en-US", {minimumIntegerDigits: 2,
         useGroupping: false}); // форматируем переменную с этажём, чтобы было 01 а не 1
-        $(".counter").text(usCurrentFloor); // записываем значение этажа в счётчик справа
+        $(".counter").text(usCurrentFloor);   // записываем значение этажа в счётчик справа
         floorPath.removeClass("current-floor"); // удаляем активный класс у этажей
         $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor"); // подсвечиваем текущий этаж
      }
@@ -31,5 +39,7 @@ $(document).ready(function () {
         $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor"); // подсвечиваем текущий этаж
         }
     });
-});
-    
+    function toggleModel() { // функция открыть-закрыть окно
+        modal.toggleClass("is-open");
+}
+});     
